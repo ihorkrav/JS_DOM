74588e8bd96440f6f30cc76f25db977baf56f3d5
IhorKrav <102153718+ihorkrav@users.noreply.github.com>
2023-09-19 12:44:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.open(innerWidth=410, innerHeight=410);
+
+ setTimeout(() => { window.resizeTo(500,500); }, 2000);
+
+ setTimeout(() => { window.moveTo(200, 200); }, 2000);
+
+ setTimeout(() => { window.close(); }, 2000);
